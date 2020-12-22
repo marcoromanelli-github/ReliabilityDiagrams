8f6c69b1e2d10c13c5c34d8f85f9f0da289a2599
marcoromanelli-github <marcoromane@gmail.com>
2020-12-22 17:55:13 +0100

    Updated test_0.py

--- ReliabilityDiagrams_test/test_0.py
+++ ReliabilityDiagrams_test/test_0.py
@@ -1,15 +1,24 @@
 import pandas as pn
-from ReliabilityDiagrams import compute_quantities, plot_reliability_diagram
+from ReliabilityDiagrams import plot_reliability_diagram
 
 
 def test_0():
-    y_true = pn.read_pickle("/tmp/RED/toy_data/y_true.pkl")
-    y_pred = pn.read_pickle("/tmp/RED/toy_data/y_pred.pkl")
+    #   load the numpy array with the true classes
+    #   m = number of samples
+    #   the numbers correspond to the class IDs
+    y_true = pn.read_pickle("/tmp/RelDiag/toy_data/y_true.pkl")
+    print(y_true)
+
+    #   load the numpy array with the probability matrices m X n
+    #   m = number of samples
+    #   n = number of classes
+    y_pred = pn.read_pickle("/tmp/RelDiag/toy_data/y_pred.pkl")
+    print(y_pred[0])
 
     plot_reliability_diagram.plot_reliabiblity_diagram(y_true=y_true,
                                                        y_pred=y_pred,
                                                        n_bins=10,
-                                                       rel_diag_folder="/tmp/RED")
+                                                       rel_diag_folder="/tmp/RelDiag")
 
     if __name__ == '__main__':
         test_0()
